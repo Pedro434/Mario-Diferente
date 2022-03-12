3522d7eed4930536196a85a9f5ef7735f191767d
Pedro434 <89937330+Pedro434@users.noreply.github.com>
2022-03-12 17:18:49 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,66 @@
 var mario
-var marioImage
+var marioRight,marioLeft,marioJump,marioIdle
 var fundo
 var nuvem1
 var nuvem2
 var imagemNuvem1
 var imagemNuvem2
+var ground,groundImage
+var lastKey = 0
 
 function preload(){
     fundo = loadImage("marioceu.png")
-    marioImage = loadAnimation("marioandano.png","marioparado.png")
+    groundImage = loadImage("marioterra.png")
+    marioIdle = loadAnimation("marioparado.png")
+    marioIdleLeft = loadAnimation("marioparadoesquerda.png")
+    marioRight = loadAnimation("marioandano.png","marioparado.png")
+    marioLeft = loadAnimation("marioandanoesquerda.png","marioparadoesquerda.png")
     imagemNuvem1 = loadImage("marionuvens1.png")
     imagemNuvem2 = loadImage("marionuvens2.png")
 }
 function setup(){
     createCanvas(800,400)
     mario=createSprite(30,259,15,35)
-    mario.addAnimation("andando",marioImage)
+    mario.addAnimation("parado",marioIdle)
+    mario.addAnimation("paradoEsquerda",marioIdleLeft)
+    mario.addAnimation("andandoDireita",marioRight)
+    mario.addAnimation("andandoEsquerda",marioLeft)
     mario.scale = 0.10
-    /*nuvem1=createSprite(200,90,24,57)
-    nuvem1.addImage(imagemNuvem1)
-    nuvem1.scale = 0.50
-    nuvem2=createSprite(600,90,24,57)
-    nuvem2.addImage(imagemNuvem2)
-    nuvem2.scale = 0.30*/
+    ground=createSprite(0,355,displayWidth,150)
+    ground.addImage("terra",groundImage)
+    ground.scale = 1.6
+    
 
 }
 function draw(){
     background(fundo)
+
+    if(keyDown("a")){
+        mario.x -= 2
+        mario.changeAnimation("andandoEsquerda",marioLeft)
+        lastKey = 1
+    }
+    if(keyDown("d")){
+        mario.x += 2
+        mario.changeAnimation("andandoDireita",marioRight)
+        lastKey = 0
+
+    }
+    if(lastKey==0){
+        mario.changeAnimation("parado",marioIdle)
+    }else{
+        mario.changeAnimation("paradoEsquerda",marioIdleLeft)
+    }
     drawSprites()
     gerarNuvens()
+    
 }
 function gerarNuvens() {
     //escreva o código aqui para gerar as nuvens 
     if (frameCount % 60 === 0) {
-        var nuvem = createSprite(200,90,24,57);
-        nuvem.velocityX = -8
-        nuvem.scale = 0.5;
+        var nuvem = createSprite(900,90,24,57);
+        nuvem.velocityX = -4
+        nuvem.scale = 0.2;
         nuvem.lifetime = 300
         
         var rand = Math.round(random(1,2));
