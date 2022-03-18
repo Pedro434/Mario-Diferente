2de51de23bef11d5e67dd9828b25cdb0daccbb2c
Pedro434 <89937330+Pedro434@users.noreply.github.com>
2022-03-18 16:30:48 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function preload(){
     marioIdleLeft = loadAnimation("marioparadoesquerda.png")
     marioRight = loadAnimation("marioandano.png","marioparado.png")
     marioLeft = loadAnimation("marioandanoesquerda.png","marioparadoesquerda.png")
+    marioJump = loadAnimation("mariopulando.png")
     imagemNuvem1 = loadImage("marionuvens1.png")
     imagemNuvem2 = loadImage("marionuvens2.png")
 }
@@ -25,11 +26,13 @@ function setup(){
     mario.addAnimation("paradoEsquerda",marioIdleLeft)
     mario.addAnimation("andandoDireita",marioRight)
     mario.addAnimation("andandoEsquerda",marioLeft)
+    mario.addAnimation("pulando",marioJump)
+
     mario.scale = 0.10
     ground=createSprite(0,355,displayWidth,150)
     ground.addImage("terra",groundImage)
     ground.scale = 1.6
-    
+    ground.debug = true
 
 }
 function draw(){
@@ -39,18 +42,26 @@ function draw(){
         mario.x -= 2
         mario.changeAnimation("andandoEsquerda",marioLeft)
         lastKey = 1
+        mario.scale = 0.10
     }
-    if(keyDown("d")){
+    else if(keyDown("d")){
         mario.x += 2
         mario.changeAnimation("andandoDireita",marioRight)
         lastKey = 0
-
+        mario.scale = 0.10
     }
-    if(lastKey==0){
+    else if(keyDown("space")){
+        mario.velocityY = -10
+        mario.changeAnimation("pulando",marioJump)
+        mario.scale = 0.18
+    }
+    else{
         mario.changeAnimation("parado",marioIdle)
-    }else{
-        mario.changeAnimation("paradoEsquerda",marioIdleLeft)
+        mario.scale = 0.10
     }
+    camera.position.x = mario.x
+    mario.velocityY = mario.velocityY + 0.8
+    mario.collide(ground)
     drawSprites()
     gerarNuvens()
     
