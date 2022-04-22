0b0e32048c4f1eac37c06b4f4e0d49272e9edcf2
Pedro434 <89937330+Pedro434@users.noreply.github.com>
2022-04-22 14:57:31 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,6 +9,7 @@ var ground,groundImage
 var lastKey = 0
 var ground2
 var goomba,goombaAnimation
+var cano,canoImg
 
 function preload(){
     fundo = loadImage("marioceu.png")
@@ -21,7 +22,8 @@ function preload(){
     marioDuck = loadAnimation("marioagachado.png")
     imagemNuvem1 = loadImage("marionuvens1.png")
     imagemNuvem2 = loadImage("marionuvens2.png")
-    goombaAnimation = loadAnimation("goomba1","goomba2")
+    goombaAnimation = loadAnimation("goomba1.png","goomba2.png")
+    canoImg = loadImage("canomario.png")
 }
 function setup(){
     createCanvas(800,400)
@@ -44,15 +46,23 @@ function setup(){
     ground2.scale = 1.6
     ground2.debug = true
 
-    goomba=createSprite(-458,259,15,15)
+    goomba=createSprite(-80,262,15,15)
     goomba.addAnimation("goombaAndando",goombaAnimation)
-    goomba.scale = 0.10
+    goomba.scale = 0.06
+    goomba.debug = true
+    goomba.velocityX = -2
+
+    cano=createSprite(30,248,15,35)
+    cano.addImage("cano",canoImg)
+    cano.scale = 0.1
 
 
 }
 function draw(){
     background(fundo)
-
+    if(goomba.x<-500){
+        goomba.velocityX = 2
+    }
     if(keyDown("LEFT_ARROW")){
         mario.x -= 4
         mario.changeAnimation("andandoEsquerda",marioLeft)
@@ -69,13 +79,13 @@ function draw(){
     }
     else if(keyDown("DOWN_ARROW")){
         mario.changeAnimation("agachado",marioDuck)
-        mario.scale = 0.1
+        mario.scale = 0.07
     }
     else if(mario.y < 259.3 ){
         mario.changeAnimation("pulando",marioJump);
         mario.scale = 0.18;
     }
-    else{
+    else if(mario.y>170||mario.collide(cano)){
         mario.changeAnimation("parado",marioIdle)
         mario.scale = 0.10
     }
@@ -86,6 +96,8 @@ function draw(){
     mario.velocityY = mario.velocityY + 0.8
     mario.collide(ground);
     mario.collide(ground2);
+    mario.collide(cano);
+    goomba.bounceOff(cano);
     console.log(mario.x)
     drawSprites()
     gerarNuvens()
@@ -95,8 +107,8 @@ function gerarNuvens() {
     //escreva o código aqui para gerar as nuvens 
     if (frameCount % 60 === 0) {
         var nuvem = createSprite(900,90,24,57);
-        nuvem.velocityX = -4
-        nuvem.scale = 0.2;
+        nuvem.velocityX = random(-1,-6);
+        nuvem.scale = random(0.1,0.8);
         nuvem.lifetime = 400
         
         var rand = Math.round(random(1,2));
